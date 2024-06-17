32858d8b1d123dc4e3883bb182c2a0ce8dc95331
Dwi Didit Prasetiyo <didit@dwidi.com>
2024-06-17 17:19:05 +0700

    Test CI/CD

--- test/test.py
+++ test/test.py
@@ -1,10 +1,11 @@
 import unittest
 from selenium import webdriver
-from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class TestMusicPlayer(unittest.TestCase):
     def setUp(self):
@@ -13,30 +14,35 @@ class TestMusicPlayer(unittest.TestCase):
         chrome_options.add_argument("--headless")  # Run without a GUI
         chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
 
-        self.driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()),
-                                       options = chrome_options)
-        
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
         self.driver.get("http://localhost:5000")
-        self.driver.implicitly_wait(30)
-    
+
     def test_play_song(self):
-        self.driver.find_element(By.XPATH, "//button[@onclick=\"playAudio(this.parentElement.getAttribute(\'data-url\'), this.parentElement)\"]").click()
-        self.driver.implicitly_wait(30)
-        
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//button[@onclick=\"playAudio(this.parentElement.getAttribute('data-url'), this.parentElement)\"]"))
+        ).click()
+
     def test_play_pause(self):
-        self.driver.find_element(By.XPATH, "//button[@onclick=\'togglePlayPause()\']").click()
-        self.driver.implicitly_wait(5)
-        self.driver.find_element(By.XPATH, "//button[@onclick=\'togglePlayPause()\']").click()
-        self.driver.implicitly_wait(30)
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//button[@onclick='togglePlayPause()']"))
+        ).click()
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//button[@onclick='togglePlayPause()']"))
+        ).click()
 
     def test_next_previous_song(self):
-        self.driver.find_element(By.XPATH, "//button[@onclick=\'nextSong()\']").click()
-        self.driver.implicitly_wait(5)
-        self.driver.find_element(By.XPATH, "//button[@onclick=\'nextSong()\']").click()
-        self.driver.implicitly_wait(5)
-        self.driver.find_element(By.XPATH, "//button[@onclick=\'previousSong()\']").click()
-        self.driver.implicitly_wait(5)
-        self.driver.find_element(By.XPATH, "//button[@onclick=\'previousSong()\']").click()
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//button[@onclick='nextSong()']"))
+        ).click()
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//button[@onclick='nextSong()']"))
+        ).click()
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//button[@onclick='previousSong()']"))
+        ).click()
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//button[@onclick='previousSong()']"))
+        ).click()
 
     def tearDown(self):
         self.driver.quit()
