b59a5e39d9ba8685f9b06472028965c397dea4fd
Dwi Didit Prasetiyo <didit@dwidi.com>
2024-06-17 17:42:18 +0700

    Test CI/CD

--- test/test.py
+++ test/test.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-from app import app
+from app import app  # Ensure the app module is correctly imported
 
 class TestMusicPlayer(unittest.TestCase):
     @classmethod
