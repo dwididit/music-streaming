f81ea8b46b2ba2d3508991bdfd9c9325569a3d3d
Dwi Didit Prasetiyo <didit@dwidi.com>
2024-06-21 20:15:29 +0700

    Test CI/CD

--- app/app.py
+++ app/app.py
@@ -7,19 +7,11 @@ import os
 app = Flask(__name__)
 
 # Setup MinIO client using environment variables
-minio_endpoint = os.getenv('MINIO_ENDPOINT')
-minio_access_key = os.getenv('MINIO_ACCESS_KEY')
-minio_secret_key = os.getenv('MINIO_SECRET_KEY')
-
-if not all([minio_endpoint, minio_access_key, minio_secret_key]):
-    raise ValueError("MinIO environment variables are not set correctly")
-
 minioClient = Minio(
-    endpoint=minio_endpoint,
-    access_key=minio_access_key,
-    secret_key=minio_secret_key
+    endpoint = os.getenv('MINIO_ENDPOINT'),
+    access_key = os.getenv('MINIO_ACCESS_KEY'),
+    secret_key =os.getenv('MINIO_SECRET_KEY')
 )
-
 
 @app.route('/')
 def index():
@@ -29,15 +21,15 @@ def index():
         music_files = []
         for obj in objects:
             # Generate a presigned URL for each music file with appropriate content type for streaming
-            url = minioClient.presigned_get_object("music-bucket", obj.object_name, expires=timedelta(hours=1),
-                                                   response_headers={"response-content-type": "audio/mpeg"})
+            url = minioClient.presigned_get_object("music-bucket", obj.object_name, expires=timedelta(hours=1), response_headers={"response-content-type": "audio/mpeg"})
             music_files.append((obj.object_name, url))
         return render_template('index.html', music_files=music_files)
-
+    
     except S3Error as exc:
         print("Error occurred.", exc)
         return "Error in fetching music files", 500
 
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug = True, 
+            host = '0.0.0.0', 
+            port = 5000)
